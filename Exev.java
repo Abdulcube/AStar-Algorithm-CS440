4dd213ff3a81d5ab92f41ff3adbf2e1fa80ccb44
BMoran32 <58581849+BMoran32@users.noreply.github.com>
2020-10-18 19:54:10 -0400

    Update Exev.java

--- Exev.java
+++ Exev.java
@@ -1,31 +1,41 @@
-
+import java.io.*;
 class Exev{
-
-  public static void main(String[] args) {
-    System.out.println("Hello, World");
-    GridGenerator test = new GridGenerator();
-  //  test.stats();
-  //  new DrawGrid(test);
-  //  Algorithm a = new Algorithm();
-  //  a.AStar();
-  //  test.writeToFile("test1.txt");
-    //int[] start = {0,0};
-    //int[] end = {159,119};
-  //  GridGenerator test = new GridGenerator();
-    //test.stats();
-    //test = new GridGenerator();
-  //  test.stats();
-  //  test.hardCells();
-
-    //test.writeToFile("test1.txt");
-    // test.stats();
-     //int[] res = GridGenerator.genEnd();
-     //System.out.println(""+ res[0]+res[1]);
-    // test.traverse();
-     //test.blockedCells();
-    // test.traverse();
-    //test.traverse();
-  //  test.writeToFile("test1.txt");
-  //  System.out.println((int)Math.random()*20);
-  }
+	public static void main(String[] args) {
+    
+		//new DrawGrid();
+		
+		//int[] s = {1,1};
+		//int[] e = {119,119};
+		
+		GridGenerator test = new GridGenerator();
+		DrawGrid b = new DrawGrid(test);
+		
+	    AlternateAlgorithm c  = new AlternateAlgorithm(test,b);
+	    //c.USearch();
+	    c.AStar();
+	    
+		
+		/*
+		GridGenerator test = new GridGenerator();
+		
+		DrawGrid a = new DrawGrid(test);
+		
+		//test.Grid[0][0].wasChecked = true;
+		
+		test.Grid[0][0].isFinalPath = true;
+		test.Grid[0][1].isFinalPath = true;
+		test.Grid[1][0].isFinalPath = true;
+		test.Grid[1][1].isFinalPath = true;
+		
+		for(int k = 10; k < 100; k++) {
+			for(int j = 60; j< 100 ; j++) {
+				if (test.Grid[k][j].type == 's' ||  test.Grid[k][j].type == 'e') {continue;} else {
+					test.Grid[k][j].wasChecked = true;
+					test.Grid[k+20][j-30].isFinalPath = true;
+					a.updateMap();		
+				}
+			}
+		}
+		*/
+	}
 }
