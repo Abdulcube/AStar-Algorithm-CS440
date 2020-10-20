bd1fb115f03e33a96cdf00edacac4408d358f430
BMoran32 <58581849+BMoran32@users.noreply.github.com>
2020-10-20 19:38:00 -0400

    Update Exev.java

--- Exev.java
+++ Exev.java
@@ -1,28 +1,50 @@
 import java.io.*;
 class Exev{
 	public static void main(String[] args) {
-		int[] x= {0,0};
-		int[] y= {100,100};
-		GridGenerator test = new GridGenerator("test5.txt");
-		test.writeToFile("test4.txt");
+    
+		long startTime = System.nanoTime();
+		long endTime   = System.nanoTime();
+		long totalTime = endTime - startTime;
+		
+		  int[] start = {0,0};
+		  int[] end = {159,119};
+		
 		//GridGenerator test = new GridGenerator("test2.txt");
-		/*GridGenerator test = new GridGenerator();
-
+		//GridGenerator test = new GridGenerator(start,end);
+		GridGenerator test = new GridGenerator();
+		  
 		test.writeToFile("test2.txt");
-
+		
 		DrawGrid b = new DrawGrid(test);
-
+		
 	    AlternateAlgorithm c  = new AlternateAlgorithm(test,b);
+	    
+	    
+	    try {
+			Thread.sleep(1000);
+		} catch (InterruptedException ie) {
+			Thread.currentThread().interrupt();
+		}
+		
+	    
 	    c.USearch();
-
+	    
+	    
 	    c.reset();
-
+	    
+	    
 	    c.AStar();
+	    
+	    
+	    c.reset();
+	    
+	    
+	    c.WAStar(.2);
+	    
+	    
 
-	    */
+	    //System.out.println("time A*: " +totalTime);
 
-
-
-
+		
 	}
 }
